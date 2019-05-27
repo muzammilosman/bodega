d1291dd340abf2706eb3073a9bf6e914b4d49bbc
muzammilosman <usman.muzammil@gmail.com>
2019-05-27 21:52:23 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserCheckComponent } from './components/user-check/user-check.component';
+import { PlaceOrderComponent } from './components/place-order/place-order.component';
+
+const appRoutes: Routes = [
+  {path: '', component: UserCheckComponent},
+  {path: 'place-order', component: PlaceOrderComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserCheckComponent,
+    PlaceOrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
